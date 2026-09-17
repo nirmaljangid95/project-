6f574e4ecdc26199bdc92398754c32bb0952f9a1
nirmal jangid <jangidnirmal15@gmail.com>
2026-09-17 21:45:33 +0530

    Add a register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -50,13 +50,13 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
-userScehma.methods.isPasswordcorreact = async function(password){
+userSchema.methods.isPasswordcorreact = async function(password){
     return await bcrypt.compare(password,this.password)
 }
-userScehma.methods.generateAceesToken = async function(){
+userSchema.methods.generateAceesToken = async function(){
     return  await jwt.sign(
     {
         _id: this._id,
@@ -70,7 +70,7 @@ userScehma.methods.generateAceesToken = async function(){
     }
 )
 }
-userScehma.methods.generateRefreshToken =  async function(){
+userSchema.methods.generateRefreshToken =  async function(){
     return await jwt.sign(
     {
         _id: this._id,
