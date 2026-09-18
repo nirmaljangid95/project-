56ed12a82cbb134de03b6b1004a2d139420eb79d
nirmal jangid <jangidnirmal15@gmail.com>
2026-09-18 23:52:03 +0530

    bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,42 +21,51 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required ")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:
             [{ username }, { email }]
     })
     if (existedUser) {
         throw new ApiError(409, "User whith email or username already exists")
     }
-    const avatarLocalPath = res.files?.avatar[0]?.path;
-    const coverImageLocalPath = res.files?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log(req.files)
+    // console.log(avatarLocalPath)
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is rewired")
+        throw new ApiError(400, "Avatar file is reqired")
     }
     const avatar = await uplodeOnClodinary(avatarLocalPath)
     const coverImage = await uplodeOnClodinary(coverImageLocalPath)
+    // console.log("avatar ", avatar);
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar image not found")
+        throw new ApiError(400, "Avatar image not nirmal papa  found")
     }
-   const user = await User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
-        coverImage : coverImage?.url || "",
+        coverImage: coverImage?.url || "",
         email,
         password,
         username: username.toLowerCase()
     })
+
+    // console.log("full name is ", user.fullName)
+    // console.log("full name is ", user.password)
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while registering the user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
-
-    return res.status(201,json(
-        new ApiResponse(200,createdUser,"User register successfully")
-    ))
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            createdUser,
+            "User registered successfully"
+        )
+    );
 
 })
 
--- src/utils/fileUplode.js
+++ src/utils/fileUplode.js
@@ -1,31 +1,31 @@
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
 
-cloudinary.config({ 
-        cloud_name: process.envCLOUDE_NAME, 
-        api_key: process.envAPI_KEY, 
-        api_secret: process.envAPI_SECRET  
-    });
-    
+cloudinary.config({
+     cloud_name: process.env.CLOUDE_NAME,
+     api_key: process.env.API_KEY,
+     api_secret: process.env.API_SECRET
+});
 
-const uplodeOnClodinary = async (localFilePath)=>{
-     try{
-      if(!localFilePath) return null
 
-      // uplode the file on cloudinary
-     const responce = await cloudinary.uploader.upload(localFilePath,{
-        resource_type :"auto"
-      })
-      console.log("file has been uploaded succ",responce.url);
-      console.log(responce);
-      return responce;
+const uplodeOnClodinary = async (localFilePath) => {
+     try {
+          if (!localFilePath) return null
+
+          // uplode the file on cloudinary
+          const responce = await cloudinary.uploader.upload(localFilePath, {
+               resource_type: "auto"
+          })
+          console.log("file has been uploaded succ", responce.url);
+          console.log(responce);
+          return responce;
      }
-    //  file has been uploaded succ
-     catch (error){
+     //  file has been uploaded succ
+     catch (error) {
           fs.unlinkSync(localFilePath)  // remove the locallu saved temporary as the upload operation got failed
           return null;
      }
 }
 
-export {uplodeOnClodinary}
+export { uplodeOnClodinary }
